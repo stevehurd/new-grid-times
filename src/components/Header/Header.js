4b4e5c21946cdfa9c222239634549e6408a9a140
Steve Hurd <660233+stevehurd@users.noreply.github.com>
2022-03-24 18:20:03 -0600

    Header Updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,70 +1,131 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES, WEIGHTS } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
-  return (
-    <header>
-      <SuperHeader>
-        <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
-          <ActionGroup>
-            <button>
-              <User size={24} />
-            </button>
-          </ActionGroup>
-        </Row>
-      </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
-    </header>
-  );
+	return (
+		<header>
+			<SuperHeader>
+				<Row>
+					<ActionGroup>
+						<button>
+							<Search size={24} />
+						</button>
+						<button>
+							<Menu size={24} />
+						</button>
+					</ActionGroup>
+					<ActionGroup>
+						<button>
+							<User size={24} />
+						</button>
+					</ActionGroup>
+				</Row>
+			</SuperHeader>
+			<MainHeader>
+				<DesktopActionGroup>
+					<button>
+						<Search size={24} />
+					</button>
+					<button>
+						<Menu size={24} />
+					</button>
+				</DesktopActionGroup>
+				<Logo />
+				<SubscriberWrapper>
+					<Button>Subscribe</Button>
+					<Sublink>Already a subscriber?</Sublink>
+				</SubscriberWrapper>
+			</MainHeader>
+		</header>
+	);
 };
 
 const SuperHeader = styled.div`
-  padding: 16px 0;
-  background: var(--color-gray-900);
-  color: white;
+	padding: 16px 0;
+	background: var(--color-gray-900);
+	color: white;
+
+	@media ${QUERIES.laptopAndUp} {
+		display: none;
+	}
 `;
 
 const Row = styled(MaxWidthWrapper)`
-  display: flex;
-  justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
 `;
 
 const ActionGroup = styled.div`
-  display: flex;
-  gap: 24px;
+	display: flex;
+	gap: 24px;
 
-  /*
+	/*
     FIX: Remove the inline spacing that comes with
     react-feather icons.
   */
-  svg {
-    display: block;
-  }
+	svg {
+		display: block;
+	}
+`;
+
+const DesktopActionGroup = styled(ActionGroup)`
+	display: none;
+
+	@media ${QUERIES.laptopAndUp} {
+		display: flex;
+	}
+`;
+
+const SubscriberWrapper = styled.div`
+	display: none;
+
+	@media ${QUERIES.laptopAndUp} {
+		display: revert;
+		position: relative;
+		justify-self: end;
+	}
+`;
+
+const Sublink = styled.a`
+	position: absolute;
+	width: 100%;
+	margin-top: 8px;
+	text-align: center;
+	font-size: 0.875rem;
+	font-style: italic;
+	font-weight: ${WEIGHTS.normal};
+	text-decoration: underline;
+	color: var(--color-gray-900);
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 32px;
-  margin-bottom: 48px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-top: 32px;
+	margin-bottom: 48px;
+
+	@media ${QUERIES.tabletAndUp} {
+		margin-top: 48px;
+		margin-bottom: 72px;
+	}
+
+	@media ${QUERIES.laptopAndUp} {
+		display: grid;
+		grid-template-columns: 1fr auto 1fr;
+		align-items: center;
+		justify-content: revert;
+		justify-items: start;
+		margin-top: 16px;
+		margin-bottom: 72px;
+	}
 `;
 
 export default Header;
